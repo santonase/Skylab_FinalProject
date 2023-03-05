d046013fa6c31bbeb0edf5b3a50699fd908708b5
Sviatoslav Binovskyi <santonase@gmail.com>
2023-03-05 12:41:16 +0200

    added func getPosters in ViewModelGenresViewController

--- TMDB/ViewModels/ViewModelGenresViewController.swift
+++ TMDB/ViewModels/ViewModelGenresViewController.swift
@@ -5,7 +5,55 @@
 //  Created by Sviatoslav Binovskyi on 03.03.2023.
 //
 
+import UIKit
+
 final class ViewModelGenresViewController {
     
     var sectionTitle: [String] = ["Trending Movies", "Trending TV", "Popular Movies", "Popular TV", "Top rated"]
+    
+    enum Sections: Int {
+        case TrendingMovies = 0
+        case TrendingTV = 1
+        case PopularMovies = 2
+        case PopularTV = 3
+        case TopRated = 4
+    }
+    
+    func getPosters(for section: Int, in cell: MediaTableViewCell) {
+        switch section {
+        case Sections.TrendingMovies.rawValue:
+                   let url = Constants.baseURL + Constants.trendingMovie + Constants.keyAPI
+                   NetworkManager().loadPosters(url: url) { response in
+                       cell.viewModel.posterArray = response
+                       cell.collectionView.reloadData()
+               }
+               case Sections.TrendingTV.rawValue:
+                   let url = Constants.baseURL + Constants.trandingTv + Constants.keyAPI
+                   NetworkManager().loadPosters(url: url) { response in
+                       cell.viewModel.posterArray = response
+                       cell.collectionView.reloadData()
+               }
+               case Sections.PopularMovies.rawValue:
+                   let url = Constants.baseURL + Constants.popularMovies + Constants.keyAPI
+                   NetworkManager().loadPosters(url: url) { response in
+                       cell.viewModel.posterArray = response
+                       cell.collectionView.reloadData()
+               }
+               case Sections.PopularTV.rawValue:
+                   let url = Constants.baseURL + Constants.popularTv + Constants.keyAPI
+       
+                   NetworkManager().loadPosters(url: url) { response in
+                       cell.viewModel.posterArray = response
+                       cell.collectionView.reloadData()
+               }
+               case Sections.TopRated.rawValue:
+                   let url = Constants.baseURL + Constants.topRated + Constants.keyAPI
+                   NetworkManager().loadPosters(url: url) { response in
+                       cell.viewModel.posterArray = response
+                       cell.collectionView.reloadData()
+               }
+        default:
+            print(Constants.nothing)
+        }
+    }
 }
--- TMDB/Views/ViewControllers/GenresViewController.swift
+++ TMDB/Views/ViewControllers/GenresViewController.swift
@@ -13,18 +13,10 @@ class GenresViewController: UIViewController {
     
     @IBOutlet weak var GenresTableView: UITableView!
     var viewModel = ViewModelGenresViewController()
-    enum Sections: Int {
-        case TrendingMovies = 0
-        case TrendingTV = 1
-        case PopularMovies = 2
-        case PopularTV = 3
-        case TopRated = 4
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
 }
 
 extension GenresViewController: UITableViewDelegate, UITableViewDataSource {
@@ -48,47 +40,8 @@ extension GenresViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.collectionViewTableViewCell) as? MediaTableViewCell else {
             return UITableViewCell()
         }
-        switch indexPath.section {
-// case 1:
-        case Sections.TrendingMovies.rawValue:
-            let url = Constants.baseURL + Constants.trendingMovie + Constants.keyAPI
-            NetworkManager().loadPosters(url: url) { response in
-                cell.viewModel.posterArray = response
-                cell.collectionView.reloadData()
-        }
-// case 2:
-        case Sections.TrendingTV.rawValue:
-            let url = Constants.baseURL + Constants.trandingTv + Constants.keyAPI
-            NetworkManager().loadPosters(url: url) { response in
-                cell.viewModel.posterArray = response
-                cell.collectionView.reloadData()
-        }
-// case 3:
-        case Sections.PopularMovies.rawValue:
-            let url = Constants.baseURL + Constants.popularMovies + Constants.keyAPI
-            NetworkManager().loadPosters(url: url) { response in
-                cell.viewModel.posterArray = response
-                cell.collectionView.reloadData()
-        }
-// case 4:
-        case Sections.PopularTV.rawValue:
-            let url = Constants.baseURL + Constants.popularTv + Constants.keyAPI
-
-            NetworkManager().loadPosters(url: url) { response in
-                cell.viewModel.posterArray = response
-                cell.collectionView.reloadData()
-        }
-// case 5:
-        case Sections.TopRated.rawValue:
-            let url = Constants.baseURL + Constants.topRated + Constants.keyAPI
-            NetworkManager().loadPosters(url: url) { response in
-                cell.viewModel.posterArray = response
-                cell.collectionView.reloadData()
-        }
-        default:
-            return UITableViewCell()
-        }
-     return cell
+        viewModel.getPosters(for: indexPath.section, in: cell)
+        return cell
     }
 }
 
